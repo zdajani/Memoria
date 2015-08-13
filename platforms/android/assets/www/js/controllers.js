562756e0e32d37b8c492a7dcfb20adbc8e806af8
Jonathan Ng <jonngkh@hotmail.com>
2015-08-13 11:28:54 +0100

    addes ios and andoroid platforms

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('starter.controllers', [])
+
+.controller('KnomiCtrl', function($scope) {})
+
+.controller('QsCtrl', function($scope) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+})
+
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
