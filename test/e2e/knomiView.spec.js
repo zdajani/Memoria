810bc057117b4a982c9a8560a3427c381abf5c0c
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-14 14:24:14 +0100

    Adding speech bubble to rock

--- test/e2e/knomiView.spec.js
+++ test/e2e/knomiView.spec.js
@@ -3,17 +3,6 @@ describe('Knomi', function() {
     browser.get('http://localhost:8100/#/tab/knomi');
   });
 
-  it('has a background-image', function() {
-    browser.sleep(5000)
-    var background = element(by.css(".gameArea"));
-    var backgroundStyle = background.getAttribute('style');
-    // console.log(backgroundStyle);
-    // expect(backgroundStyle.getCssValue('background-image').toEqual('url("../img/field.png"'))
-    // expect(element(by.css(".gameArea")).toContain()
-  }.then(function() {
-    expect(backgroundStyle.getCssValue('background-image').toEqual('url("../img/field.png"'))
-  }));
-
 
 });
 
