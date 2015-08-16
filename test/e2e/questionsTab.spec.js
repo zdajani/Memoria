f8b013e413945749f909f32747fc85f4b832d4e2
zdajani <dajani.zeina@gmail.com>
2015-08-16 13:59:51 +0100

    view questions on tab protractor test

--- test/e2e/questionsTab.spec.js
+++ test/e2e/questionsTab.spec.js
@@ -10,5 +10,11 @@ describe('questions tab', function(){
     goToAddQuestion.click();
     expect(browser.getLocationAbsUrl()).toMatch("/add-question");
   });
+  
+  it('should have the questions added listed', function(){
+    var questionList = element.all(by.repeater('questions'));
+    expect(questionList.get(0).getText()).toContain("What is the capital of Palestine?");
+    expect(questionList.get(1).getText()).toContain("What is the capital of England?");
+  })
 
 });
